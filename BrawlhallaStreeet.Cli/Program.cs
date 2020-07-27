90b312cd75c49327925fd5dc09b67ae2169f46ec
Killerwalski <pakowalski86@gmail.com>
2020-07-26 23:56:27 -0400

    Read insert working

--- BrawlhallaStreeet.Cli/Program.cs
+++ BrawlhallaStreeet.Cli/Program.cs
@@ -1,13 +1,22 @@
-﻿using Serilog;
+﻿using BrawlhallaStreet.Core;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Serilog;
 using Serilog.Events;
 using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BrawlhallaStreeet.Cli
 {
-    internal class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        private IConfiguration Configuration;
+        private IMongoCollection<BrawlhallaPlayer> MongoCollection;
+        private static async Task Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
            .MinimumLevel.Debug()
@@ -20,13 +29,52 @@ namespace BrawlhallaStreeet.Cli
             Console.WriteLine("Hello World!");
             Log.Information("Application Starting");
 
+            
+
             Program prog = new Program();
+            await prog.Setup();
 
         }
 
-        public async Task UpdatePlayer()
+        public async Task Setup()
         {
+            Configuration = new ConfigurationBuilder()
+                // .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.Development.json", true, true)
+                .Build();
 
+            var connectionString = Configuration["BrawlhallaDatabaseSettings:ConnectionString"];
+            var databaseName = Configuration["BrawlhallaDatabaseSettings:DatabaseName"];
+            var collectionName = Configuration["BrawlhallaDatabaseSettings:CollectionName"];
+
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            MongoCollection = database.GetCollection<BrawlhallaPlayer>(collectionName);
+
+            var count = await MongoCollection.CountDocumentsAsync(new BsonDocument());
+            if (count == 0)
+                await SeedDatabase();
+
+        }
+
+        public async Task<BrawlhallaPlayer> GetBrawlhallaPlayer(int playerId, IConfiguration config)
+        {
+            var url = $"https://api.brawlhalla.com/player/{playerId}/stats?api_key=" + config["BrawlhallaApiKey"];
+            HttpClient client = new HttpClient();
+            string responseBody = await client.GetStringAsync(url);
+            var player = JsonConvert.DeserializeObject<BrawlhallaPlayer>(responseBody);
+            return player;
+        }
+
+        private async Task SeedDatabase()
+        {
+            var document = JsonConvert.DeserializeObject<BrawlhallaPlayer>(File.ReadAllText(@"Killerwalski_Seed.json"));
+            await MongoCollection.InsertOneAsync(document);
+        }
+
+        public async Task InsertPlayer(BrawlhallaPlayer player)
+        {
+            await MongoCollection.InsertOneAsync(player);
         }
     }
 }
--- BrawlhallaStreet.Tests/ApiTests.cs
+++ BrawlhallaStreet.Tests/ApiTests.cs
@@ -1,12 +1,46 @@
-using System;
+using BrawlhallaStreeet.Cli;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace BrawlhallaStreet.Tests
 {
     public class ApiTests
     {
+        public ILogger Logger;
+        public IConfiguration Configuration;
+
+        public ApiTests(ITestOutputHelper output)
+        {
+            Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.TestOutput(output, Serilog.Events.LogEventLevel.Verbose)
+                .CreateLogger()
+                .ForContext<ApiTests>();
+
+            Configuration = new ConfigurationBuilder()
+                // .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile(@"C:\Code\Repositories\BrawlhallaStreet\BrawlhallaStreeet.Cli\appsettings.Development.json", true, true)
+                .Build();
+        }
+
         [Fact]
-        public void Test1()
+        public async Task Insert_Updated_Player_ToMongoAsync()
+        {
+            Program prog = new Program();
+            await prog.Setup();
+
+            var result = await prog.GetBrawlhallaPlayer(3879460, Configuration);
+            Logger.Information("Result: " + result);
+
+            Assert.NotNull(result);
+            await prog.InsertPlayer(result);
+        }
+
+        [Fact]
+        public void Calculate_Difference_Between_Last_Two_Players()
         {
 
         }
