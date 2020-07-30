61bc07995b109215f67d57eb65f707ed593615a5
Killerwalski <pakowalski86@gmail.com>
2020-07-30 00:49:07 -0400

    Fake

---
+++ BrawlhallaStreet.Core/Services/FakeDataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrawlhallaStreet.Core.Services
+{
+    class FakeDataService : IDataService
+    {
+        public Task<BrawlhallaPlayer> GetBrawlhallaPlayer(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<BrawlhallaPlayer>> GetLatestEntriesForPlayer(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertPlayer(BrawlhallaPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
